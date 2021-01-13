86a102f62e0ba81bfa49a9cf0eb0073713461242
OSformula <69432458+OSformula@users.noreply.github.com>
2021-01-13 19:34:07 +0100

    Create EasyJointing_mmd_tools.py

---
+++ EasyJointing_mmd_tools.py
@@ -0,0 +1,26 @@
+#Blender 2.8 plugin
+#
+#
+#Original by OSformula
+
+import bpy
+context = bpy.context
+selection_names = bpy.context.selected_objects
+
+first_element = selection_names[0].name
+second_element = selection_names[1].name
+
+print(first_element)
+print(second_element)
+
+bpy.context.scene.tool_settings.transform_pivot_point = 'MEDIAN_POINT'
+
+bpy.context.area.type = 'VIEW_3D'
+bpy.ops.view3d.snap_cursor_to_selected()
+
+bpy.ops.mmd_tools.joint_add()
+
+bpy.context.object.rigid_body_constraint.object1 = bpy.data.objects[first_element]
+bpy.context.object.rigid_body_constraint.object2 = bpy.data.objects[second_element]
+
+bpy.context.area.type = 'TEXT_EDITOR'
